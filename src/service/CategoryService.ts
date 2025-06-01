1e53b29c1ec24284901750de9049565435f7c244
Ashish Gupta <ashishg0926@gmail.com>
2025-06-01 22:35:57 +0530

    Update CategoryService api calls

--- src/service/CategoryService.ts
+++ src/service/CategoryService.ts
@@ -8,17 +8,30 @@ export const addCategory = async (category: Omit<Category, "categoryId" | "creat
         formData.append('file', file);
     }
     console.log(formData);
-    const response = await axios.post('http://localhost:8080/v1/categories', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' }
+    const response = await axios.post('http://localhost:8080/v1/admin/categories', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'Authorization': `Bearer ${import.meta.env.VITE_BEARER_TOKEN}`,
+        }
     });
     return response.data;
 };
 
 export const deleteCategory = async (categoryId: string) => {
-    return await axios.delete<void>(`http://localhost:8080/v1/categories/${categoryId}`);
+    return await axios.delete<void>(`http://localhost:8080/v1/admin/categories/${categoryId}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${import.meta.env.VITE_BEARER_TOKEN}`,
+        }
+    });
 }
 
 export const fetchCategories = async (): Promise<Category[]> => {
-    const response = await axios.get<Category[]>('http://localhost:8080/v1/categories');
+    const response = await axios.get<Category[]>('http://localhost:8080/v1/categories', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${import.meta.env.VITE_BEARER_TOKEN}`,
+        },
+    });
     return response.data;
 }
